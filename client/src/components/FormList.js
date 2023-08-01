a4b983268563747f9e0c6078659f2640954b4f34
Gayathri-S-26 <85394882+Gayathri-S-26@users.noreply.github.com>
2023-08-01 16:13:14 +0530

    Add files via upload

---
+++ client/src/components/FormList.js
@@ -0,0 +1,53 @@
+//FormList.js
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import FormPreview from './FormPreview';
+
+const FormList = () => {
+  const [forms, setForms] = useState([]);
+  const [previewForm, setPreviewForm] = useState(null);
+
+
+  useEffect(() => {
+    // Fetch all forms from the backend and set them to the forms state
+    fetchForms();
+  }, []);
+
+  const fetchForms = () => {
+    axios.get('http://localhost:5000/forms')
+      .then((response) => {
+        setForms(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching forms:', error);
+      });
+  };
+
+  const handleFormPreview = (form) => {
+    alert("See below for the Preview");
+    setPreviewForm(form);
+  };
+
+  return (
+    <div>
+      {forms.map((form) => (
+        <div key={form._id}>
+          <ul>
+          <li><h3>{form.title}</h3></li>
+          </ul>
+          <button onClick={() => handleFormPreview(form)}>Preview</button>
+        </div>
+      ))}
+
+     {previewForm && (
+        <div>
+          <h1>Form Preview</h1>
+          <FormPreview form={previewForm} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FormList;

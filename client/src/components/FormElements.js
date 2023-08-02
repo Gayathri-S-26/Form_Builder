96d7f59ad08bb62c2295bebc4b48ad8631e79e2c
Gayathri-S-26 <85394882+Gayathri-S-26@users.noreply.github.com>
2023-08-02 16:29:47 +0530

    Update FormElements.js

--- client/src/components/FormElements.js
+++ client/src/components/FormElements.js
@@ -8,9 +8,52 @@ const FormElements = ({ elements }) => {
     <div>
       {elements.map((element, index) => (
         <div key={index}>
-          <li key={index}>{element}</li>
           <label>{element.label}</label>
-          {/* Add input fields for different element types */}
+          {element.type === 'text' && (
+            <input type="text" />
+          )}
+          {element.type === 'number' && (
+            <input type="number" />
+          )}
+          {element.type === 'email' && (
+            <input type="email" />
+          )}
+          {element.type === 'date' && (
+            <input type="date" />
+          )}
+          {element.type === 'password' && (
+            <input type="password" />
+          )}
+          {element.type === 'radio' && (
+            <div>
+              {element.options.map((option, i) => (
+                <div key={i}>
+                  <input type="radio" name={element.label} value={option} />
+                  <label>{option}</label>
+                </div>
+              ))}
+            </div>
+          )}
+          {element.type === 'checkbox' && (
+            <div>
+              {element.options.map((option, i) => (
+                <div key={i}>
+                  <input type="checkbox" name={element.label} value={option} />
+                  <label>{option}</label>
+                </div>
+              ))}
+            </div>
+          )}
+          {element.type === 'dropdown' && (
+            <select>
+              {element.options.map((option, i) => (
+                <option key={i} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          )}
+          {/* Add more cases for other element types as needed */}
         </div>
       ))}
     </div>
